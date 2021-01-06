4f7001bf3066252e42d8e19ac124240c659df972
felipeeC <felipe.moraes.alves@hotmail.com>
2021-01-06 17:50:27 -0300

    mapeando file e pessoa, excluindo classe filmepessoa

--- src/main/java/br/com/compasso/lambda/desafioCompasso/models/Pessoa.java
+++ src/main/java/br/com/compasso/lambda/desafioCompasso/models/Pessoa.java
@@ -1,6 +1,13 @@
 package br.com.compasso.lambda.desafioCompasso.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa {
+	@Id  @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String nome;
 	private int idade;
