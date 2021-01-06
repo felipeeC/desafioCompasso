0b04b4a849cd764de544f2443b255f1f14c6dde2
felipeeC <felipe.moraes.alves@hotmail.com>
2021-01-06 15:49:10 -0300

    init

---
+++ src/main/java/br/com/compasso/lambda/desafioCompasso/DesafioCompassoApplication.java
@@ -0,0 +1,13 @@
+package br.com.compasso.lambda.desafioCompasso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCompassoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCompassoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/compasso/lambda/desafioCompasso/DesafioCompassoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compasso.lambda.desafioCompasso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCompassoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
