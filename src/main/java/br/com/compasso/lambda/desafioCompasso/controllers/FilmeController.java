bbb764bc495c6cce3aaef071c48b5b92315ce30a
felipeeC <felipe.moraes.alves@hotmail.com>
2021-01-07 16:00:49 -0300

    Criação filme repository + filmeDto + listagem de todos os filmes

--- src/main/java/br/com/compasso/lambda/desafioCompasso/controllers/FilmeController.java
+++ src/main/java/br/com/compasso/lambda/desafioCompasso/controllers/FilmeController.java
@@ -1,10 +1,29 @@
 package br.com.compasso.lambda.desafioCompasso.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.compasso.lambda.desafioCompasso.dtos.FilmeDto;
+import br.com.compasso.lambda.desafioCompasso.models.Filme;
+import br.com.compasso.lambda.desafioCompasso.repositories.FilmeRepository;
 
 @RestController
 @RequestMapping(value = "/filmes")
 public class FilmeController {
 
+	@Autowired
+	private FilmeRepository filmeRepository;
+	
+	@RequestMapping
+	public List<FilmeDto> filmes(){
+		List<Filme> filmes = filmeRepository.findAll();
+				
+		return FilmeDto.converter(filmes);
+	}
+	
+	
+	
 }
---
+++ src/main/java/br/com/compasso/lambda/desafioCompasso/dtos/FilmeDto.java
@@ -0,0 +1,35 @@
+package br.com.compasso.lambda.desafioCompasso.dtos;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.compasso.lambda.desafioCompasso.models.Filme;
+
+public class FilmeDto {
+
+	private Long id;
+	private String descricao;
+	private String nome;
+
+	public FilmeDto(Filme filme) {
+		this.id = filme.getId();
+		this.descricao = filme.getDescricao();
+		this.nome = filme.getNome();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public static List<FilmeDto> converter(List<Filme> filmes) {
+		return filmes.stream().map(FilmeDto::new).collect(Collectors.toList());
+	}
+}
