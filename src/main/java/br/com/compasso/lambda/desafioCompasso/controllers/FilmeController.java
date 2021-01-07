11bd3b28cd2c1efcb5fa8f9062bed1fb4428ed45
felipeeC <felipe.moraes.alves@hotmail.com>
2021-01-07 17:38:33 -0300

    Adicionando get post de filme (adiciona e remove)

--- src/main/java/br/com/compasso/lambda/desafioCompasso/controllers/FilmeController.java
+++ src/main/java/br/com/compasso/lambda/desafioCompasso/controllers/FilmeController.java
@@ -1,12 +1,19 @@
 package br.com.compasso.lambda.desafioCompasso.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.compasso.lambda.desafioCompasso.dtos.FilmeDto;
+import br.com.compasso.lambda.desafioCompasso.dtos.FilmeForm;
 import br.com.compasso.lambda.desafioCompasso.models.Filme;
 import br.com.compasso.lambda.desafioCompasso.repositories.FilmeRepository;
 
@@ -17,13 +24,19 @@ public class FilmeController {
 	@Autowired
 	private FilmeRepository filmeRepository;
 	
-	@RequestMapping
+	@GetMapping
 	public List<FilmeDto> filmes(){
 		List<Filme> filmes = filmeRepository.findAll();
 				
 		return FilmeDto.converter(filmes);
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<FilmeDto> cadastrar(@RequestBody FilmeForm form, UriComponentsBuilder uriBuilder) {
+		Filme filme = form.converter();
+		filmeRepository.save(filme);
+		URI uri = uriBuilder.path("/filmes/{id}").buildAndExpand(filme.getId()).toUri();
+		return ResponseEntity.created(uri).body(new FilmeDto(filme));
+	}
 	
 }
