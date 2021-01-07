c9e48512981d0ac86d2096fa1ab4059c25c6f0d1
guilhermecalderaro <guilhermecald96@gmail.com>
2021-01-07 17:38:58 -0300

    Adicionado pessoas no data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO Filme(descricao, nome, comentario, data_lancamento, estudio, diretor, elenco) VALUES ('A bordo', 'Titanic','Filme trágico',  '1997-05-23', 'Hollywood',  'James Cameron', 'Leonardo Dicaprio');
+INSERT INTO Filme(descricao, nome, comentario, data_lancamento, estudio, diretor, elenco)
+	VALUES ('A bordo', 'Titanic','Filme trágico',  '1997-05-23', 'Hollywood',  'James Cameron', 'Leonardo Dicaprio');
+
+INSERT INTO Pessoa(nome, idade)
+	VALUES ('Guilherme', 24), ('Rafael', 21), ('Gabriel', 27);
