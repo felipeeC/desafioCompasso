b19214228decd44ae1022d2981480c6d573459c1
Gimble123 <etgesstertz@gmail.com>
2021-01-08 18:06:21 -0300

    Criando método delete e post.

---
+++ bin/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Filme(descricao, nome, comentario, data_lancamento, estudio, diretor, elenco)
+	VALUES ('A bordo', 'Titanic','Filme trágico',  '1997-05-23', 'Hollywood',  'James Cameron', 'Leonardo Dicaprio');
+
+INSERT INTO Pessoa(nome, idade)
+	VALUES ('Guilherme', 24), ('Rafael', 21), ('Gabriel', 27);
+    
+INSERT INTO Categoria(nome)
+     VALUES ('Suspense'), ('Comédia'), ('Ação'), ('Aventura'), ('Infantil'), ('Romance'), ('Ficção'), ('Terror'), ('Mistério'), ('Drama');
