037e141cf564a729e54a792871f95d4c42c81878
Gimble123 <etgesstertz@gmail.com>
2021-01-11 14:38:53 -0300

    Resolvendo conflito.

--- src/main/java/br/com/compasso/lambda/desafioCompasso/controllers/FilmeController.java
+++ src/main/java/br/com/compasso/lambda/desafioCompasso/controllers/FilmeController.java
@@ -17,10 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.compasso.lambda.desafioCompasso.dtos.FilmeCompletoDto;
 import br.com.compasso.lambda.desafioCompasso.dtos.FilmeDto;
 import br.com.compasso.lambda.desafioCompasso.dtos.FilmeForm;
 import br.com.compasso.lambda.desafioCompasso.models.Filme;
-import br.com.compasso.lambda.desafioCompasso.repositories.FilmeRepository;
 import br.com.compasso.lambda.desafioCompasso.services.FilmeService;
 
 @RestController
@@ -35,13 +35,20 @@ public class FilmeController {
 		List<Filme> filmes = filmeService.getFilmes();
 		return FilmeDto.converter(filmes);
 	}
+	
+	@GetMapping(value = "/completo")
+	public List<FilmeCompletoDto> filmesCompletos() {
+		List<Filme> filmes = filmeService.getFilmes();
+		return FilmeCompletoDto.converter(filmes);
+	}
+
 
 	@PostMapping
-	public ResponseEntity<FilmeDto> cadastrar(@RequestBody @Valid FilmeForm form, UriComponentsBuilder uriBuilder) {
+	public  ResponseEntity<FilmeCompletoDto> cadastrar(@RequestBody @Valid FilmeForm form, UriComponentsBuilder uriBuilder) {
 		Filme filme = form.converter();
-		filmeService.postFilme(filme);
-		URI uri = uriBuilder.path("/filmes/{id}").buildAndExpand(filme.getId()).toUri();
-		return ResponseEntity.created(uri).body(new FilmeDto(filme));
+		return filmeService.postFilme(filme, uriBuilder);
+		
+		//return ResponseEntity.created(uri).body(new FilmeCompletoDto(filme));
 	}
 	
 	@PutMapping(value = "/{id}")
@@ -57,5 +64,4 @@ public class FilmeController {
 	}
 	
 	
-
 }
--- src/main/java/br/com/compasso/lambda/desafioCompasso/services/FilmeService.java
+++ src/main/java/br/com/compasso/lambda/desafioCompasso/services/FilmeService.java
@@ -1,12 +1,16 @@
 package br.com.compasso.lambda.desafioCompasso.services;
 
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.compasso.lambda.desafioCompasso.dtos.FilmeCompletoDto;
 import br.com.compasso.lambda.desafioCompasso.models.Filme;
 import br.com.compasso.lambda.desafioCompasso.repositories.FilmeRepository;
 @Service
@@ -14,19 +18,30 @@ public class FilmeService {
 	
 	@Autowired
 	private FilmeRepository filmeRepository;
-	
-	//private List<Filme> filmes = new ArrayList<Filme>();
+
+	// private List<Filme> filmes = new ArrayList<Filme>();
 
 	// Métodos
-	
+
 	@Transactional(readOnly = true)
 	public List<Filme> getFilmes() {
-		 List<Filme> filmes = filmeRepository.findAll();
-		 return filmes;
+		List<Filme> filmes = filmeRepository.findAll();
+		return filmes;
 	}
-	 
-	public void postFilme(Filme filme) {
-		filmeRepository.save(filme);
+
+	public ResponseEntity<FilmeCompletoDto> postFilme(Filme filme, UriComponentsBuilder uriBuilder) {
+		List<Filme> filmes = getFilmes();
+
+		if (filmes.contains(filme)) {
+			System.out.println("num deu");
+			
+			return ResponseEntity.notFound().build();
+		} else {
+			filmeRepository.save(filme);
+			URI uri = uriBuilder.path("/filmes/{id}").buildAndExpand(filme.getId()).toUri();
+			return ResponseEntity.created(uri).body(new FilmeCompletoDto(filme));
+		}
+
 	}
 	
 	public Filme update(Long id, Filme obj) {
@@ -45,40 +60,7 @@ public class FilmeService {
 		
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 //	public void imprimeById(int idFilme) {
 //		for (Filme filme : filmes) {
 //			if (filme.getId() == idFilme) {
