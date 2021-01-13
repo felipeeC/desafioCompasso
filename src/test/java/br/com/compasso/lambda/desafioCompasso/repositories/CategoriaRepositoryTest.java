3ad5b52881df9269a02404bd3bc18810612e6ae5
Augusto <augustoschweickardt@gmail.com>
2021-01-13 15:45:03 -0300

    Criando classes de testes de PessoaRepositoryTest, CategoriaRepositoryTest e PessoaServiceTest.

---
+++ src/test/java/br/com/compasso/lambda/desafioCompasso/repositories/CategoriaRepositoryTest.java
@@ -0,0 +1,15 @@
+package br.com.compasso.lambda.desafioCompasso.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+
+public class CategoriaRepositoryTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
