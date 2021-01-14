703b9684a684f76961ef23ab732eb4ce6a1c6d77
guilhermecalderaro <guilhermecald96@gmail.com>
2021-01-14 16:52:58 -0300

    Adicionado os testes unitários de PessoaController

--- src/main/java/br/com/compasso/lambda/desafioCompasso/dtos/PessoaForm.java
+++ src/main/java/br/com/compasso/lambda/desafioCompasso/dtos/PessoaForm.java
@@ -7,14 +7,17 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import br.com.compasso.lambda.desafioCompasso.models.Pessoa;
 
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class PessoaForm {
 	
 	@NotNull @NotEmpty @Length(min = 5, max = 60)
 	private String nome;
 	
-	@NotNull @DecimalMax(value = "110") @DecimalMin(value = "0")
+	@NotNull @DecimalMax(value = "110") @DecimalMin(value = "1")
 	private int idade;
 	
 	public Pessoa converter() {
@@ -27,6 +30,14 @@ public class PessoaForm {
 
 	public void setIdade(int idade) {
 		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
 	}
 	
 	
---
+++ src/test/java/br/com/compasso/lambda/desafioCompasso/controllers/PessoaControllerTest.java
@@ -0,0 +1,178 @@
+package br.com.compasso.lambda.desafioCompasso.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.compasso.lambda.desafioCompasso.dtos.PessoaForm;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class PessoaControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	final static ObjectMapper mapper = new ObjectMapper();
+	
+	@Test
+	void testCadastroPessoaNomeVazio() throws Exception {
+		
+		PessoaForm pessoaForm = new PessoaForm();
+		
+		pessoaForm.setNome("");
+		pessoaForm.setIdade(24);
+
+		// setting fields for the NewObject  
+
+		URI uri = new URI("/pessoas");
+		
+		mockMvc.perform(MockMvcRequestBuilders.post(uri)
+		  .content(asJsonString(pessoaForm))
+		  .contentType(MediaType.APPLICATION_JSON))
+		  .andExpect(MockMvcResultMatchers.status().is(HttpStatus.BAD_REQUEST.value()));
+		  
+	}
+	
+	@Test
+	void testCadastroPessoaNomeNulo() throws Exception {
+		
+		PessoaForm pessoaForm = new PessoaForm();
+		pessoaForm.setIdade(24);
+		// setting fields for the NewObject  
+		URI uri = new URI("/pessoas");
+		
+		mockMvc.perform(MockMvcRequestBuilders.post(uri)
+		  .content(asJsonString(pessoaForm))
+		  .contentType(MediaType.APPLICATION_JSON))
+		  .andExpect(MockMvcResultMatchers.status().is(HttpStatus.BAD_REQUEST.value()));
+		  
+		 
+	}
+	
+	@Test
+	void testCadastroPessoaIdadeNulo() throws Exception {
+		
+		PessoaForm pessoaForm = new PessoaForm();
+		pessoaForm.setNome("Guilherme");
+		
+		// setting fields for the NewObject  
+		URI uri = new URI("/pessoas");
+		
+		mockMvc.perform(MockMvcRequestBuilders.post(uri)
+		  .content(asJsonString(pessoaForm))
+		  .contentType(MediaType.APPLICATION_JSON))
+		  .andExpect(MockMvcResultMatchers.status().is(HttpStatus.BAD_REQUEST.value()));
+		  
+		 
+	}
+	
+	@Test
+	void testCadastroPessoaIdadeNegativa() throws Exception {
+		
+		PessoaForm pessoaForm = new PessoaForm();
+		pessoaForm.setNome("Guilherme");
+		pessoaForm.setIdade(-24);
+		
+		// setting fields for the NewObject  
+		URI uri = new URI("/pessoas");
+		
+		mockMvc.perform(MockMvcRequestBuilders.post(uri)
+		  .content(asJsonString(pessoaForm))
+		  .contentType(MediaType.APPLICATION_JSON))
+		  .andExpect(MockMvcResultMatchers.status().is(HttpStatus.BAD_REQUEST.value()));
+		  
+		 
+	}
+	
+	@Test
+	void testCadastroPessoaIdadeMaiorQuePermitido() throws Exception {
+		
+		PessoaForm pessoaForm = new PessoaForm();
+		pessoaForm.setNome("Guilherme");
+		pessoaForm.setIdade(125);
+		
+		// setting fields for the NewObject  
+		URI uri = new URI("/pessoas");
+		
+		mockMvc.perform(MockMvcRequestBuilders.post(uri)
+		  .content(asJsonString(pessoaForm))
+		  .contentType(MediaType.APPLICATION_JSON))
+		  .andExpect(MockMvcResultMatchers.status().is(HttpStatus.BAD_REQUEST.value()));
+		  
+		 
+	}
+	
+	@Test
+	void testCadastroPessoaCompleto() throws Exception {
+		
+		PessoaForm pessoaForm = new PessoaForm();
+		// setting fields for the NewObject  
+
+		pessoaForm.setNome("Marcelo");
+		pessoaForm.setIdade(55);
+		
+		URI uri = new URI("/pessoas");
+		
+		mockMvc.perform(MockMvcRequestBuilders.post(uri)
+		  .content(asJsonString(pessoaForm))
+		  .contentType(MediaType.APPLICATION_JSON))
+		  .andExpect(MockMvcResultMatchers.status().is(HttpStatus.CREATED.value()));
+		 
+	}
+
+	@Test
+	void testGetPessoa() throws Exception {
+		URI uri = new URI("/pessoas/1");
+		
+		mockMvc.perform(get(uri)
+		.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.nome", Matchers.is("Guilherme")))
+			.andExpect(jsonPath("$.idade", Matchers.is(24)));
+	}
+	
+	@Test
+	void testGetTodasPessoas() throws Exception {
+		URI uri = new URI("/pessoas");
+		
+		mockMvc.perform(get(uri)
+		.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$[0].nome", Matchers.is("Guilherme")))
+			.andExpect(jsonPath("$[0].idade", Matchers.is(24)));
+	}
+	
+	public static String asJsonString(final Object obj) {
+	    try {
+	    		
+	        final String jsonContent = mapper.writeValueAsString(obj);
+	        return jsonContent;
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
+	}  
+
+}
