5423e5a9083518092c9d97dbdfd3243bc21c460f
Adriana Silva <adrianasilva487@yahoo.com>
2021-01-13 21:44:45 -0300

    Criado classe CategoriaControllerTest com testes do método cadastrar categoria

---
+++ src/test/java/br/com/compasso/lambda/desafioCompasso/controllers/CategoriaControllerTest.java
@@ -0,0 +1,96 @@
+package br.com.compasso.lambda.desafioCompasso.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.URI;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.compasso.lambda.desafioCompasso.dtos.CategoriaForm;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class CategoriaControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	void testCadastroCategoriaNomeVazio() throws Exception {
+		
+		CategoriaForm categoriaForm = new CategoriaForm();
+		
+		categoriaForm.setNome("");
+		// setting fields for the NewObject  
+
+		URI uri = new URI("/categorias");
+		
+		mockMvc.perform(MockMvcRequestBuilders.post(uri)
+		  .content(asJsonString(categoriaForm))
+		  .contentType(MediaType.APPLICATION_JSON))
+		  .andExpect(MockMvcResultMatchers.status().is(HttpStatus.NO_CONTENT.value()));
+		  
+		 
+	}
+	
+	@Test
+	void testCadastroCategoriaNomeNulo() throws Exception {
+		
+		CategoriaForm categoriaForm = new CategoriaForm();
+				
+		// setting fields for the NewObject  
+		URI uri = new URI("/categorias");
+		
+		mockMvc.perform(MockMvcRequestBuilders.post(uri)
+		  .content(asJsonString(categoriaForm))
+		  .contentType(MediaType.APPLICATION_JSON))
+		  .andExpect(MockMvcResultMatchers.status().is(HttpStatus.NO_CONTENT.value()));
+		  
+		 
+	}
+	
+	@Test
+	void testCadastroCategoriaPossuiNome() throws Exception {
+		
+		CategoriaForm categoriaForm = new CategoriaForm();
+		// setting fields for the NewObject  
+
+		categoriaForm.setNome("João");
+		
+		URI uri = new URI("/categorias");
+		
+		mockMvc.perform(MockMvcRequestBuilders.post(uri)
+		  .content(asJsonString(categoriaForm))
+		  .contentType(MediaType.APPLICATION_JSON))
+		  .andExpect(MockMvcResultMatchers.status().is(HttpStatus.CREATED.value()));
+		  
+		 
+	}
+	
+	
+	public static String asJsonString(final Object obj) {
+	    try {
+	        final ObjectMapper mapper = new ObjectMapper();
+	        final String jsonContent = mapper.writeValueAsString(obj);
+	        return jsonContent;
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    }
+	}  
+
+}
