818acca572c983da1e9b94065130e934506db225
Adriana Silva <adrianasilva487@yahoo.com>
2021-01-14 10:14:01 -0300

    Classe CategoriaServiceTest

---
+++ src/test/java/br/com/compasso/lambda/desafioCompasso/services/CategoriaServiceTest.java
@@ -0,0 +1,14 @@
+package br.com.compasso.lambda.desafioCompasso.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CategoriaServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
