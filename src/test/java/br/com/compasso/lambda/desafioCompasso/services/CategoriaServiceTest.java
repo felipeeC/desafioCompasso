cc1c8f6ce8b1b18c52888c37e5bfd60e974ce52a
Adriana Silva <adrianasilva487@yahoo.com>
2021-01-14 12:30:25 -0300

    Teste método findAll em CategoriaService

--- src/test/java/br/com/compasso/lambda/desafioCompasso/services/CategoriaServiceTest.java
+++ src/test/java/br/com/compasso/lambda/desafioCompasso/services/CategoriaServiceTest.java
@@ -2,12 +2,41 @@ package br.com.compasso.lambda.desafioCompasso.services;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Assert;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import br.com.compasso.lambda.desafioCompasso.models.Categoria;
 
+@SpringBootTest
+
 class CategoriaServiceTest {
+	
+	@Autowired
+	private CategoriaService service;
+	
+	@Test
+	public void retornaTodasCategorias() {
+
+		List<Categoria> categorias = service.getCategorias();
+
+		Categoria categoria1 = new Categoria("Suspense");
+		Categoria categoria2 = new Categoria("Comédia");
+		Categoria categoria3 = new Categoria("Ação");
+		
+		List<Categoria> categoriasTeste = new ArrayList<>();
+
+		categoriasTeste.add(categoria1);
+		categoriasTeste.add(categoria2);
+		categoriasTeste.add(categoria3);
+
+		assertEquals(categoria1.getNome(), categorias.get(0).getNome());
+		assertEquals(categoria2.getNome(), categorias.get(1).getNome());
+		assertEquals(categoria3.getNome(), categorias.get(2).getNome());
+	}
 
 	@Test
 	public void testFindOne() throws Exception {
@@ -22,10 +51,10 @@ class CategoriaServiceTest {
 		// Recuperando as informações da mesma
 		//Categoria categoria = restTemplate.getForObject(BASE_PATH + "/" + categorias.get(1).getIdCategoria(), Categoria.class);
 
-		// Verificamos se o resultado da requisição é igual ao esperado
-		// Se sim significa que tudo correu bem
-		//Assert.assertNotNull(categoria);
-		//Assert.assertEquals("Terror", categoria.getFirstName());
-		//Assert.assertEquals("Comédia", categoria.getLastName());
+		 //Verificamos se o resultado da requisição é igual ao esperado
+		 //Se sim significa que tudo correu bem
+		 //Assert.assertNotNull(categoria);
+		 //Assert.assertEquals("Terror", categoria.getFirstName());
+		 //Assert.assertEquals("Comédia", categoria.getLastName());
 	}
 }
