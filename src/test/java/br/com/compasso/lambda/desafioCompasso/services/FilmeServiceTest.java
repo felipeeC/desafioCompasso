536576bd30aa6273e62fbf9fb3bd3dd5b6df85b2
Augusto Schweickardt <augustoschweickardt@gmail.com>
2021-02-03 15:13:45 -0300

    Adicionando o perfil de teste ao FilmeServiceTest

--- src/test/java/br/com/compasso/lambda/desafioCompasso/services/FilmeServiceTest.java
+++ src/test/java/br/com/compasso/lambda/desafioCompasso/services/FilmeServiceTest.java
@@ -11,11 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import br.com.compasso.lambda.desafioCompasso.dtos.FilmeForm;
 import br.com.compasso.lambda.desafioCompasso.models.Filme;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class FilmeServiceTest {
