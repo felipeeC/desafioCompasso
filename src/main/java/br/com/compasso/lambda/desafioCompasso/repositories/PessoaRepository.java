8abbbbf4c5453c113fedc481433e83db5290c820
guilhermecalderaro <guilhermecald96@gmail.com>
2021-02-03 20:43:40 -0300

    retirado imports não utilizados e espaços extras

--- src/main/java/br/com/compasso/lambda/desafioCompasso/repositories/PessoaRepository.java
+++ src/main/java/br/com/compasso/lambda/desafioCompasso/repositories/PessoaRepository.java
@@ -3,17 +3,11 @@ package br.com.compasso.lambda.desafioCompasso.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import br.com.compasso.lambda.desafioCompasso.models.Filme;
 import br.com.compasso.lambda.desafioCompasso.models.Pessoa;
-
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 	
 	Optional<Pessoa> findById(Long id);
-	
-	
 
 }
